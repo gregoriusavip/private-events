7391bf24f7aaefa26b9f0f1accd43037e56cee08
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2025-05-20 17:00:01 -0700

    Fix show page only showing the current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
-    @events = current_user.created_events
+    @events = User.find(params[:id]).created_events
   end
 end
